7020045e92b19140d096be14794a44898d2a8686
Jeevana <jeevana.pasam@gmail.com>
2025-07-04 21:56:11 +0530

    java fundamentals

---
+++ java_fundamentals/src/java_fundamentals/Question5.java
@@ -0,0 +1,24 @@
+package java_fundamentals;
+
+import java.util.Scanner;
+
+public class Question5 {
+
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		int n1 = sc.nextInt();
+		int n2 = sc.nextInt();
+		
+		if ( n1 % 10 == n2 % 10)
+		{
+			System.out.println("True");
+		}
+		else
+		{
+			System.out.println("False");
+		}
+
+	}
+
+}
