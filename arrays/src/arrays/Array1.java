c44fc357a12dd1ea02cdb824e7fd498507e02749
Jeevana <jeevana.pasam@gmail.com>
2025-07-06 17:14:45 +0530

    arrays

---
+++ arrays/src/arrays/Array1.java
@@ -0,0 +1,20 @@
+package arrays;
+import java.util.*;
+
+public class Array1 
+{
+	public static void main(String[] args)
+	{
+		int[] numbers = {10, 20, 30, 40, 50};
+		int  sum = 0;
+		
+		for (int i=0; i<numbers.length; i++) 
+		{
+			sum = sum + numbers[i];
+		}
+		double average = (double) sum / numbers.length;
+		System.out.println("Sum: "+sum);
+		System.out.println("Average: "+average);
+	}
+
+}
---
+++ arrays/src/arrays/Array4.java
@@ -0,0 +1,17 @@
+package arrays;
+import java.util.*;
+
+public class Array4 {
+
+	public static void main(String[] args) 
+	{
+		int[] asciiValues = {11, 12, 13, 14, 15}; 
+
+        System.out.println("Characters: ");
+        for (int value : asciiValues) {
+            System.out.print((char) value);
+        }
+
+	}
+
+}
